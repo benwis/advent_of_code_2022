4298121f81bf9c9f8c9d7110bfebfdb311fced7d
Ben Wishovich <ben@benw.is>
2022-12-06 09:59:25 -0800

    Not too bad this time

--- src/day06/part1.rs
+++ src/day06/part1.rs
@@ -1,5 +1,20 @@
+use std::collections::HashSet;
+
 use crate::day06::{Input, Output};
 
+pub fn check_window(w: &[char]) -> bool {
+    let check = w.iter().copied().collect::<HashSet<char>>();
+    check.len() == 4
+}
+
 pub fn solve(input: &Input) -> Output {
-    unimplemented!()
+    let args = input.windows(4).map(check_window).collect::<Vec<bool>>();
+    let mut ans = 0;
+    for (i, val) in args.iter().enumerate() {
+        if *val {
+            ans = i + 4;
+            break;
+        }
+    }
+    Output::U32(ans as u32)
 }
--- src/day06/part2.rs
+++ src/day06/part2.rs
@@ -1,5 +1,20 @@
 use crate::day06::{Input, Output};
 
+use std::collections::HashSet;
+
+pub fn check_window(w: &[char]) -> bool {
+    let check = w.iter().copied().collect::<HashSet<char>>();
+    check.len() == 14
+}
+
 pub fn solve(input: &Input) -> Output {
-    unimplemented!()
+    let args = input.windows(14).map(check_window).collect::<Vec<bool>>();
+    let mut ans = 0;
+    for (i, val) in args.iter().enumerate() {
+        if *val {
+            ans = i + 14;
+            break;
+        }
+    }
+    Output::U32(ans as u32)
 }
